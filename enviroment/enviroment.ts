ce6de414c1315a0ba4fe9be5acf8c17d0ba3bf1c
Alex Oliveira <contatoalexandergv@gmail.com>
2024-10-30 14:38:32 -0300

    atualizado api para netlify

--- enviroment/enviroment.ts
+++ enviroment/enviroment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:3000',  // URL da API de desenvolvimento
+    apiUrl: 'https://api-gerenciamento-estoque.netlify.app/api',  // URL da API de desenvolvimento
 };
