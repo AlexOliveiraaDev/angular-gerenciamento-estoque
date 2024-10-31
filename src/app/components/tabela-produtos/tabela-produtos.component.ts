bc7ae7934128e749ce2dc648acdb127e9dac7534
Alex Oliveira <contatoalexandergv@gmail.com>
2024-10-30 23:06:27 -0300

    func adicionar e remover produtos

--- src/app/components/tabela-produtos/tabela-produtos.component.ts
+++ src/app/components/tabela-produtos/tabela-produtos.component.ts
@@ -9,9 +9,9 @@ import { CheckboxModule } from 'primeng/checkbox';
 import { DialogModule } from 'primeng/dialog';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
-
-
-
+import { DropdownModule } from 'primeng/dropdown';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { FormsModule } from '@angular/forms';
 
 interface Produto {
   id: number;
@@ -22,10 +22,19 @@ interface Produto {
   localizacao: string;
 }
 
+interface Item {
+  produto: string;
+  categoria: string;
+  quantidade: number;
+  preco: number;
+  localizacao: string;
+  [key: string]: string | number;
+}
+
 @Component({
   selector: 'app-tabela-produtos',
   standalone: true,
-  imports: [TableModule, SpeedDialModule, IconFieldModule, InputIconModule, CheckboxModule, DialogModule, ButtonModule, InputTextModule],
+  imports: [TableModule, SpeedDialModule, IconFieldModule, InputIconModule, CheckboxModule, DialogModule, ButtonModule, InputTextModule, DropdownModule, InputNumberModule, FormsModule],
   templateUrl: './tabela-produtos.component.html',
   styleUrl: './tabela-produtos.component.scss',
   providers: []
@@ -33,6 +42,26 @@ interface Produto {
 export class TabelaProdutosComponent implements OnInit {
 
   visible = false;
+  categorias = [
+    { name: "Categoria 01" },
+    { name: "Categoria 02" }
+  ];
+  localizacao = [
+    { name: "Categoria 01" },
+    { name: "Categoria 02" }
+  ];
+
+  deleteItem: any;
+  quantidadeVenda: number = 1;
+  displayVenda: boolean = false;
+
+  item: Item = {
+    produto: '',
+    categoria: '',
+    quantidade: 0,
+    preco: 0,
+    localizacao: '',
+  };
 
   title = 'gerenciador-estoque';
   listaProdutos: Produto[] = [];
@@ -54,11 +83,75 @@ export class TabelaProdutosComponent implements OnInit {
       this.listaProdutos = res;
     });
   }
+
+  newProduto() {
+    this.http.post<any>(environment.apiUrl + '/produtos', this.item).subscribe({
+      next: (res) => {
+        console.log('Produto adicionado com sucesso:', res);
+        this.getProdutos();
+      },
+      error: (error) => {
+        console.error('Erro ao adicionar produto:', error);
+        // Lide com o erro, exiba uma mensagem para o usuário, etc.
+      }
+    });
+  }
+
+  deleteProduto(id: number) {
+    this.http.delete<any>(`${environment.apiUrl}/produtos/${id}`).subscribe({
+      next: (res) => {
+        console.log('Produto excluído com sucesso:', res);
+        this.getProdutos(); // Atualiza a lista de produtos após a exclusão
+      },
+      error: (error) => {
+        console.error('Erro ao excluir produto:', error);
+        // Lide com o erro, exiba uma mensagem para o usuário, etc.
+      }
+    });
+  }
+
+
   ngOnInit(): void {
     this.getProdutos()
   }
 
   ShowDialog() {
     this.visible = !this.visible;
+  }
+
+  AdicionarProduto() {
+    console.log(this.item)
+    this.newProduto();
+  }
+
+
+  displayConfirmation: boolean = false;
+
+  showConfirmDialog(item: any) {
+    this.deleteItem = item; // Define o item a ser excluído
+    this.displayConfirmation = true;
+  }
+
+  confirmDelete() {
+    // Lógica para excluir o item (usando this.item)
+    console.log('Item excluído:', this.deleteItem);
+    this.deleteProduto(this.deleteItem.id)
+    this.displayConfirmation = false;
+  }
+
+
+
+
+
+  showVendaDialog(item: any) {
+    this.item = item;
+    this.quantidadeVenda = 1; // Resetar quantidade ao abrir
+    this.displayVenda = true;
+  }
+
+  confirmarVenda() {
+    // Lógica para registrar a venda (usando this.item e this.quantidadeVenda)
+    console.log(`Venda de ${this.quantidadeVenda} unidades de ${this.item.produto} confirmada!`);
+    this.displayVenda = false;
   }
 }
